3a92b972674554f40f053984ee55db7e4ac66011
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2025-11-13 12:34:19 +0200

    FIX: Preprocessor event propagation with look-ahead in EEG buffer

--- ros2_ws/src/neurosimo_packages/pipeline/preprocessor/src/preprocessor_wrapper.h
+++ ros2_ws/src/neurosimo_packages/pipeline/preprocessor/src/preprocessor_wrapper.h
@@ -54,11 +54,12 @@ public:
   bool process(
       eeg_msgs::msg::PreprocessedSample& output_sample,
       const RingBuffer<std::shared_ptr<eeg_msgs::msg::Sample>>& buffer,
-      double_t sample_time,
+      double_t sample_window_base_time,
       bool pulse_given);
 
   WrapperState get_state() const;
   std::size_t get_buffer_size() const;
+  int get_look_ahead_samples() const;
 
   /* Exposed to Python, defined in cpp_bindings.cpp. */
   static void log(const std::string& message);
@@ -89,8 +90,8 @@ private:
   std::unique_ptr<py::array_t<double>> py_eeg_data;
   std::unique_ptr<py::array_t<double>> py_emg_data;
 
-  int earliest_sample;
-  int latest_sample;
+  int look_back_samples;
+  int look_ahead_samples;
   uint16_t sampling_frequency;
 
   std::size_t buffer_size = 0;

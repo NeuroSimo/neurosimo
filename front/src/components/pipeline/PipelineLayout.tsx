02ab6265e0eb90c2e4fa809331f51c0b99a7b9fb
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2025-12-05 18:05:36 +0200

    MOD: Make PipelineView use two columns

--- front/src/components/pipeline/PipelineLayout.tsx
+++ front/src/components/pipeline/PipelineLayout.tsx
@@ -1,13 +1,41 @@
 import React from 'react'
 import styled from 'styled-components'
 
+type Props = {
+  setup: React.ReactNode
+  pipeline: React.ReactNode
+}
+
 const Layout = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-template-rows: auto 1fr;
+  gap: 16px 24px;
+  align-items: start;
 `
 
-export const PipelineLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
-  return <Layout>{children}</Layout>
+const SetupArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  grid-column: 1;
+  grid-row: 1 / span 2;
+`
+
+const PipelineArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  grid-column: 2;
+  grid-row: 1 / span 2;
+`
+
+export const PipelineLayout: React.FC<Props> = ({ setup, pipeline }) => {
+  return (
+    <Layout>
+      <SetupArea>{setup}</SetupArea>
+      <PipelineArea>{pipeline}</PipelineArea>
+    </Layout>
+  )
 }
 

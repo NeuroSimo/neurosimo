58cb10f7b6bff1cce2f19af81b541a44f83054ef
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2025-12-05 19:02:15 +0200

    MOD: Lots of changes to main UI layout

--- front/src/components/ProjectSelector.tsx
+++ front/src/components/ProjectSelector.tsx
@@ -1,15 +1,20 @@
 import React, { useContext, useEffect, useState } from 'react'
 import styled from 'styled-components'
 
-import { ProjectRow, Select } from 'styles/General'
+import { ProjectRow, Select, StyledPanel, SmallerTitle, CONFIG_PANEL_WIDTH } from 'styles/General'
 import { listProjects, setActiveProject } from 'ros/project'
 import { ProjectContext } from 'providers/ProjectProvider'
+
+const Container = styled(StyledPanel)`
+  width: ${CONFIG_PANEL_WIDTH}px;
+  position: relative;
+`
 
 const Label = styled.label`
   width: 92px;
   text-align: left;
   margin-right: 5px;
-  margin-left: 18px;
+  margin-left: 0;
   display: inline-block;
 `
 
@@ -29,16 +34,19 @@ export const ProjectSelector: React.FC = () => {
   }
 
   return (
-    <ProjectRow>
-      <Label>Project:</Label>
-      <Select onChange={handleChange} value={activeProject}>
-        {projects.map((project, index) => (
-          <option key={index} value={project}>
-            {project}
-          </option>
-        ))}
-      </Select>
-    </ProjectRow>
+    <Container>
+      <SmallerTitle>Project</SmallerTitle>
+      <ProjectRow>
+        <Label>Project:</Label>
+        <Select onChange={handleChange} value={activeProject}>
+          {projects.map((project, index) => (
+            <option key={index} value={project}>
+              {project}
+            </option>
+          ))}
+        </Select>
+      </ProjectRow>
+    </Container>
   )
 }
 

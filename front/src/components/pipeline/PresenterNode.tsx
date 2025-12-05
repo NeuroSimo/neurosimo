67c3f672dd5885871b215b267f483995bb7ad4fc
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2025-12-05 15:08:30 +0200

    CLN: Modularize PipelineView in UI

---
+++ front/src/components/pipeline/PresenterNode.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { StyledPanel, SmallerTitle, ConfigRow, ConfigLabel, Select } from 'styles/General'
+import { ToggleSwitch } from 'components/ToggleSwitch'
+
+type Props = {
+  enabled: boolean
+  module: string
+  modules: string[]
+  onToggle: (enabled: boolean) => void
+  onModuleChange: (event: React.ChangeEvent<HTMLSelectElement>) => void
+}
+
+const Container = styled(StyledPanel)`
+  width: 154px;
+  height: 92px;
+`
+
+export const PresenterNode: React.FC<Props> = ({ enabled, module, modules, onToggle, onModuleChange }) => {
+  return (
+    <Container>
+      <SmallerTitle>Presenter</SmallerTitle>
+      <ConfigRow>
+        <ConfigLabel>Enabled:</ConfigLabel>
+        <ToggleSwitch type='flat' checked={enabled} onChange={onToggle} />
+      </ConfigRow>
+      <ConfigRow>
+        <ConfigLabel>Module:</ConfigLabel>
+        <Select onChange={onModuleChange} value={module}>
+          {modules.map((mod, index) => (
+            <option key={index} value={mod}>
+              {mod}
+            </option>
+          ))}
+        </Select>
+      </ConfigRow>
+    </Container>
+  )
+}
+

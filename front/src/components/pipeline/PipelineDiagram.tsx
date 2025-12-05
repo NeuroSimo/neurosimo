d2bb9ae85bf2d68a93cd88bf76746145dea47636
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2025-12-05 17:53:04 +0200

    CLN: Further extraction of components from PipelineView

---
+++ front/src/components/pipeline/PipelineDiagram.tsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { PreprocessorNode } from 'components/pipeline/PreprocessorNode'
+import { DeciderNode } from 'components/pipeline/DeciderNode'
+import { PresenterNode } from 'components/pipeline/PresenterNode'
+import { TmsNode } from 'components/pipeline/TmsNode'
+import { PipelineConnections } from 'components/pipeline/PipelineConnections'
+
+const PipelinePanel = styled.div`
+  display: grid;
+  grid-template-rows: repeat(2, 1fr);
+  grid-template-columns: repeat(4, 1fr);
+  width: 370px;
+  height: 277px;
+  gap: 30px;
+  position: relative;
+  margin-left: 18px;
+`
+
+const PreprocessorSlot = styled.div`
+  grid-row: 1 / 2;
+  grid-column: 2 / 3;
+`
+
+const DeciderSlot = styled.div`
+  grid-row: 1 / 2;
+  grid-column: 3 / 4;
+`
+
+const PresenterSlot = styled.div`
+  grid-row: 1 / 2;
+  grid-column: 4 / 5;
+`
+
+const TmsSlot = styled.div`
+  grid-row: 2 / 3;
+  grid-column: 4 / 5;
+`
+
+const EegCircle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  grid-row: 1 / 2;
+  grid-column: 1 / 2;
+  width: 44px;
+  height: 44px;
+  margin-top: 46px;
+  background-color: #d46c0b;
+  border-radius: 50%;
+  font-weight: bold;
+`
+
+export const PipelineDiagram: React.FC = () => (
+  <PipelinePanel>
+    <PipelineConnections />
+    <EegCircle>EEG</EegCircle>
+    <PreprocessorSlot>
+      <PreprocessorNode />
+    </PreprocessorSlot>
+    <DeciderSlot>
+      <DeciderNode />
+    </DeciderSlot>
+    <PresenterSlot>
+      <PresenterNode />
+    </PresenterSlot>
+    <TmsSlot>
+      <TmsNode />
+    </TmsSlot>
+  </PipelinePanel>
+)
+

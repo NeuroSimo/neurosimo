6fb4086940e03ef1f140a47dfd32ab1e59151f7d
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2025-12-10 15:29:33 +0200

    MOD: Start and stop streaming by service calls to EEG bridge

--- ros2_ws/src/eeg/eeg_bridge/src/eeg_bridge.h
+++ ros2_ws/src/eeg/eeg_bridge/src/eeg_bridge.h
@@ -14,10 +14,11 @@
 
 #include "pipeline_interfaces/msg/latency_measurement_trigger.hpp"
 
-#include "system_interfaces/msg/session.hpp"
-#include "system_interfaces/msg/session_state.hpp"
 #include "system_interfaces/msg/healthcheck.hpp"
 #include "system_interfaces/msg/healthcheck_status.hpp"
+#include "system_interfaces/msg/streamer_state.hpp"
+
+#include "std_srvs/srv/trigger.hpp"
 
 #include "adapters/eeg_adapter.h"
 
@@ -76,15 +77,17 @@ private:
   bool check_for_dropped_samples(uint64_t device_sample_index);
 
   void create_publishers();
-  void create_subscribers();
 
   void publish_eeg_healthcheck();
   void publish_eeg_info();
+  void publish_streamer_state();
 
-  void subscribe_to_session();
-
-  void stop_session();
-  void wait_for_session();
+  void handle_start_streamer(
+      const std::shared_ptr<std_srvs::srv::Trigger::Request> request,
+      std::shared_ptr<std_srvs::srv::Trigger::Response> response);
+  void handle_stop_streamer(
+      const std::shared_ptr<std_srvs::srv::Trigger::Request> request,
+      std::shared_ptr<std_srvs::srv::Trigger::Response> response);
 
   /* Configuration */
   uint16_t port = 0;
@@ -101,34 +104,28 @@ private:
   rclcpp::Publisher<eeg_interfaces::msg::Sample>::SharedPtr eeg_sample_publisher;
   rclcpp::Publisher<eeg_interfaces::msg::EegInfo>::SharedPtr eeg_info_publisher;
   rclcpp::Publisher<system_interfaces::msg::Healthcheck>::SharedPtr healthcheck_publisher;
+  rclcpp::Publisher<system_interfaces::msg::StreamerState>::SharedPtr streamer_state_publisher;
   rclcpp::Publisher<pipeline_interfaces::msg::LatencyMeasurementTrigger>::SharedPtr latency_measurement_trigger_publisher;
 
   rclcpp::TimerBase::SharedPtr healthcheck_publisher_timer;
 
-  /* Subscribers */
-  rclcpp::Subscription<system_interfaces::msg::Session>::SharedPtr session_subscriber;
+  /* Services */
+  rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr start_streamer_service;
+  rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr stop_streamer_service;
 
-  /* Session management */
-  bool first_sample_of_session = true;
+  /* Streaming state */
+  system_interfaces::msg::StreamerState::_state_type streamer_state = system_interfaces::msg::StreamerState::READY;
+  bool is_session_start = false;
+  bool is_session_end = false;
   
   /* Device sample tracking for dropped sample detection */
   uint64_t previous_device_sample_index = UNSET_PREVIOUS_SAMPLE_INDEX;
   
-  /* Session sample index (starts at 0 for each session) */
-  uint64_t session_sample_index = 0;
+  /* Streaming sample index (starts at 0 for each streaming run) */
+  uint64_t streaming_sample_index = 0;
 
   double_t time_offset = UNSET_TIME;     // in seconds
-  double_t session_start_time = UNSET_TIME;  // Unix timestamp when session started
-
-  bool session_been_stopped = true;
-  bool session_received = false;
-  system_interfaces::msg::SessionState session_state;
-
-  /* Session management */
-
-  /* Initialize 'wait_for_session_to_stop' to true to avoid starting streaming when EEG bridge is restarted in the middle
-     of a session, while EEG device is already streaming. TODO: Is this required now that mTMS device is not supported? */
-  bool wait_for_session_to_stop = true;
+  double_t streaming_start_time = UNSET_TIME;  // Unix timestamp when streaming started
 
   /* Healthcheck */
   uint8_t status = system_interfaces::msg::HealthcheckStatus::NOT_READY;

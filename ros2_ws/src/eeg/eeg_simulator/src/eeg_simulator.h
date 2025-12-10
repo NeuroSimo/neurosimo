d2c22143856d55b0c829916f3e2ae0de99c06e09
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2025-12-10 15:35:14 +0200

    CLN: Improve variable naming in EEG bridge and EEG simulator

--- ros2_ws/src/eeg/eeg_simulator/src/eeg_simulator.h
+++ ros2_ws/src/eeg/eeg_simulator/src/eeg_simulator.h
@@ -97,8 +97,8 @@ private:
   uint8_t num_emg_channels;
   uint8_t total_channels;
 
-  double_t streaming_start_time = UNSET_TIME;  // Unix timestamp when streaming started
-  uint64_t streaming_sample_index = 0;         // Monotonic sample index within a streaming run
+  double_t session_start_time = UNSET_TIME;  // Unix timestamp when streaming started
+  uint64_t session_sample_index = 0;         // Monotonic sample index within a streaming run
   bool is_session_start = false;
   bool is_session_end = false;
 

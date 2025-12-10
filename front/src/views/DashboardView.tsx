1e16e87f75fc9e4fde74a01b1cda4e5f1f3bf01b
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2025-12-10 13:28:47 +0200

    DEL: Session-related functionality from UI

--- front/src/views/DashboardView.tsx
+++ front/src/views/DashboardView.tsx
@@ -3,14 +3,12 @@ import React from 'react'
 import { EegStreamDisplay } from 'components/EegStreamDisplay'
 import { StatisticsDisplay } from 'components/StatisticsDisplay'
 import { StimulationDisplay } from 'components/StimulationDisplay'
-import { SessionDisplay } from 'components/SessionDisplay'
 import { ExperimentStatePanel } from 'components/pipeline/ExperimentStatePanel'
 import { PipelineLogDisplay } from 'components/PipelineLogDisplay'
 
 export const DashboardView = () => {
   return (
     <>
-      <SessionDisplay />
       <EegStreamDisplay />
       <StatisticsDisplay />
       <StimulationDisplay />

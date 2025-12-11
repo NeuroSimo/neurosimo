a205e6e60871c0a33bca00f95603a82bd6938bd1
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2025-12-11 21:18:25 +0200

    MOD: Move EegDeviceDisplay to PipelineView

--- front/src/views/DashboardView.tsx
+++ front/src/views/DashboardView.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-import { EegStreamDisplay } from 'components/EegStreamDisplay'
 import { StatisticsDisplay } from 'components/StatisticsDisplay'
 import { StimulationDisplay } from 'components/StimulationDisplay'
 import { ExperimentStatePanel } from 'components/pipeline/ExperimentStatePanel'
@@ -9,7 +8,6 @@ import { PipelineLogDisplay } from 'components/PipelineLogDisplay'
 export const DashboardView = () => {
   return (
     <>
-      <EegStreamDisplay />
       <StatisticsDisplay />
       <StimulationDisplay />
       <ExperimentStatePanel />

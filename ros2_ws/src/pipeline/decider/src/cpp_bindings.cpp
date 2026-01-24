7cd56d40063feb72a3f3515c41394212a5fc7166
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2026-01-24 18:21:07 +0200

    FIX: Pybind build by linking Python and wrapper into shared core

--- ros2_ws/src/pipeline/decider/src/cpp_bindings.cpp
+++ ros2_ws/src/pipeline/decider/src/cpp_bindings.cpp
@@ -1,36 +1,33 @@
 #include <pybind11/pybind11.h>
-#include <pybind11/embed.h>
 #include <pybind11/stl.h>
-#include <pybind11/numpy.h>
 
 #include "decider_wrapper.h"
 
 namespace py = pybind11;
 
 void DeciderWrapper::log(const std::string& message) {
-  /* Buffer the log message to avoid ROS2 publishing overhead during Python execution */
   std::lock_guard<std::mutex> lock(log_buffer_mutex);
   log_buffer.push_back({message, LogLevel::INFO});
 }
 
 void DeciderWrapper::log_throttle(const std::string& message, const double_t period) {
   static std::unordered_map<std::string, double_t> last_log_times;
-  
-  double_t current_time = rclcpp::Clock().now().seconds();
-  
+
+  const double_t current_time = rclcpp::Clock().now().seconds();
+
   auto it = last_log_times.find(message);
   if (it != last_log_times.end() && current_time - it->second < period) {
     return;
   }
-  
-  /* Buffer the throttled log message */
+
   std::lock_guard<std::mutex> lock(log_buffer_mutex);
   log_buffer.push_back({"[Throttled] " + message, LogLevel::INFO});
-  
+
   last_log_times[message] = current_time;
 }
 
 PYBIND11_MODULE(cpp_bindings, m) {
-    m.def("log", &DeciderWrapper::log);
-    m.def("log_throttle", &DeciderWrapper::log_throttle);
+  m.def("log", &DeciderWrapper::log, py::arg("message"));
+  m.def("log_throttle", &DeciderWrapper::log_throttle,
+        py::arg("message"), py::arg("period") = 1.0);
 }

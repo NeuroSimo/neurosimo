85a0f7c90fb362cf8aa77c3ae3d4ff1d9f0373fa
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2026-01-25 13:56:08 +0200

    MOD: Improvements to resource monitor

--- ros2_ws/src/system/resource_monitor/ros_entrypoint.sh
+++ ros2_ws/src/system/resource_monitor/ros_entrypoint.sh
@@ -4,4 +4,6 @@ set -e
 source /opt/ros/iron/setup.bash
 source /app/ros2_ws/install/setup.bash
 
-ros2 launch resource_monitor resource_monitor.launch.py log-level:="$ROS_LOG_LEVEL"
+ros2 launch resource_monitor resource_monitor.launch.py log-level:="$ROS_LOG_LEVEL" \
+    disk-warning-threshold:="$DISK_WARNING_THRESHOLD" \
+    disk-error-threshold:="$DISK_ERROR_THRESHOLD"

75ef8075a24e513eca0281bf14c90fad726b160c
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2026-01-25 13:56:08 +0200

    MOD: Improvements to resource monitor

--- ros2_ws/src/system/resource_monitor/launch/resource_monitor.launch.py
+++ ros2_ws/src/system/resource_monitor/launch/resource_monitor.launch.py
@@ -12,7 +12,19 @@ def generate_launch_description():
         description="Logging level",
     )
 
+    disk_warning_threshold_arg = DeclareLaunchArgument(
+        "disk-warning-threshold",
+        description="Warning threshold for disk space (e.g., 25GiB, 500MiB)",
+    )
+
+    disk_error_threshold_arg = DeclareLaunchArgument(
+        "disk-error-threshold",
+        description="Error threshold for disk space (e.g., 20GiB, 100MiB)",
+    )
+
     logger = LaunchConfiguration("log-level")
+    disk_warning_threshold = LaunchConfiguration("disk-warning-threshold")
+    disk_error_threshold = LaunchConfiguration("disk-error-threshold")
 
     node_executables = [
         "resource_monitor",
@@ -22,10 +34,18 @@ def generate_launch_description():
         node = Node(
             package="resource_monitor",
             executable=node_executable,
+            parameters=[
+                {
+                    "disk.warning_threshold": disk_warning_threshold,
+                    "disk.error_threshold": disk_error_threshold,
+                }
+            ],
             arguments=['--ros-args', '--log-level', logger]
         )
         ld.add_action(node)
 
     ld.add_action(log_arg)
+    ld.add_action(disk_warning_threshold_arg)
+    ld.add_action(disk_error_threshold_arg)
 
     return ld

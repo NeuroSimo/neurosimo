c2d134577c51133beb6a56c6040dc4338f431c67
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2026-01-26 23:48:36 +0200

    MOD: UI: Add detached view to show experiment state

--- front/public/preload.js
+++ front/public/preload.js
@@ -2,4 +2,5 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
   openProjectFolder: (projectName, subdir) => ipcRenderer.invoke('open-project-folder', projectName, subdir),
+  openDetachedExperimentWindow: () => ipcRenderer.invoke('open-detached-experiment-window'),
 });

4b34247747626b6d0737d3a5a8865b96d04e3987
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2026-01-26 23:25:27 +0200

    MOD: UI: Add 'folder' icon to open recordings folder

--- front/public/preload.js
+++ front/public/preload.js
@@ -1,14 +1,5 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-// Expose protected methods that allow the renderer process to use
-// the ipcRenderer without exposing the entire object
 contextBridge.exposeInMainWorld('electronAPI', {
-  // Example: Add methods here if you need to communicate between
-  // main and renderer processes. For now, this is just a placeholder.
-
-  // minimize: () => ipcRenderer.invoke('minimize-window'),
-  // maximize: () => ipcRenderer.invoke('maximize-window'),
-  // close: () => ipcRenderer.invoke('close-window'),
-
-  // You can add more API methods as needed for your app's functionality
+  openRecordingsFolder: (projectName) => ipcRenderer.invoke('open-recordings-folder', projectName),
 });

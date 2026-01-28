9b8c3652df333de3f4102300da85f95efdb6b2f0
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2026-01-28 16:16:26 +0200

    MOD: Delete outdated 'DROPPED_SAMPLE_THRESHOLD' env variable

--- ros2_ws/src/pipeline/preprocessor/ros_entrypoint.sh
+++ ros2_ws/src/pipeline/preprocessor/ros_entrypoint.sh
@@ -4,5 +4,4 @@ set -e
 source /opt/ros/iron/setup.bash
 source /app/ros2_ws/install/setup.bash
 
-ros2 launch preprocessor preprocessor.launch.py log-level:="$ROS_LOG_LEVEL" \
-    dropped-sample-threshold:="$DROPPED_SAMPLE_THRESHOLD"
+ros2 launch preprocessor preprocessor.launch.py log-level:="$ROS_LOG_LEVEL"

642755d357945f84c339b8feafc2b21c866a8a92
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2026-02-09 19:59:51 +0200

    MOD: Use global config to pass disk space thresholds

--- ros2_ws/src/system/resource_monitor/ros_entrypoint.sh
+++ ros2_ws/src/system/resource_monitor/ros_entrypoint.sh
@@ -4,6 +4,4 @@ set -e
 source /opt/ros/iron/setup.bash
 source /app/ros2_ws/install/setup.bash
 
-ros2 launch resource_monitor resource_monitor.launch.py log-level:="$ROS_LOG_LEVEL" \
-    disk-warning-threshold:="$DISK_WARNING_THRESHOLD" \
-    disk-error-threshold:="$DISK_ERROR_THRESHOLD"
+ros2 launch resource_monitor resource_monitor.launch.py log-level:="$ROS_LOG_LEVEL"

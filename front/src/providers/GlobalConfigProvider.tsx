20e920d8fc830c58dfda3c9d97c8542c6f5a4664
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2026-02-09 13:38:35 +0200

    MOD: UI: Hook up config to backend

--- front/src/providers/GlobalConfigProvider.tsx
+++ front/src/providers/GlobalConfigProvider.tsx
@@ -5,7 +5,19 @@ import { ros } from 'ros/ros'
 
 interface GlobalConfigContextType {
   activeProject: string
+  eegPort: number
+  eegDevice: string
+  turbolinkSamplingFrequency: number
+  turbolinkEegChannelCount: number
+  maximumDroppedSamples: number
+  simulateLabjack: boolean
+  minimumIntertrialInterval: number
+  maximumLoopbackLatency: number
+  maximumTimingError: number
+  diskWarningThreshold: string
+  diskErrorThreshold: string
   setActiveProject: (project: string, callback?: () => void) => Promise<void>
+  setGlobalConfig: (config: Partial<Omit<GlobalConfigContextType, 'setActiveProject' | 'setGlobalConfig'>>, callback?: () => void) => Promise<void>
 }
 
 // ESLint disable for intentionally empty functions used as defaults
@@ -15,7 +27,19 @@ const asyncNoop = async () => {}
 
 const defaultGlobalConfigState: GlobalConfigContextType = {
   activeProject: '',
+  eegPort: 0,
+  eegDevice: '',
+  turbolinkSamplingFrequency: 0,
+  turbolinkEegChannelCount: 0,
+  maximumDroppedSamples: 0,
+  simulateLabjack: false,
+  minimumIntertrialInterval: 0,
+  maximumLoopbackLatency: 0,
+  maximumTimingError: 0,
+  diskWarningThreshold: '',
+  diskErrorThreshold: '',
   setActiveProject: asyncNoop,
+  setGlobalConfig: asyncNoop,
 }
 
 export const GlobalConfigContext = createContext<GlobalConfigContextType>(defaultGlobalConfigState)
@@ -25,9 +49,33 @@ interface GlobalConfigProviderProps {
 }
 
 export const GlobalConfigProvider: React.FC<GlobalConfigProviderProps> = ({ children }) => {
-  const [globalConfig, setGlobalConfig] = useState<Map<string, string>>(new Map())
-
-  const activeProject = globalConfig.get('active_project') || ''
+  const [globalConfig, setGlobalConfig] = useState<{
+    activeProject: string
+    eegPort: number
+    eegDevice: string
+    turbolinkSamplingFrequency: number
+    turbolinkEegChannelCount: number
+    maximumDroppedSamples: number
+    simulateLabjack: boolean
+    minimumIntertrialInterval: number
+    maximumLoopbackLatency: number
+    maximumTimingError: number
+    diskWarningThreshold: string
+    diskErrorThreshold: string
+  }>({
+    activeProject: '',
+    eegPort: 0,
+    eegDevice: '',
+    turbolinkSamplingFrequency: 0,
+    turbolinkEegChannelCount: 0,
+    maximumDroppedSamples: 0,
+    simulateLabjack: false,
+    minimumIntertrialInterval: 0,
+    maximumLoopbackLatency: 0,
+    maximumTimingError: 0,
+    diskWarningThreshold: '',
+    diskErrorThreshold: '',
+  })
 
   useEffect(() => {
     /* Subscriber for global config topic (latched). */
@@ -40,15 +88,19 @@ export const GlobalConfigProvider: React.FC<GlobalConfigProviderProps> = ({ chil
 
     globalConfigSubscriber.subscribe((message: ROSLIB.Message) => {
       const msg = message as any
-      setGlobalConfig((prevConfig) => {
-        // Only update if active_project has changed
-        if (prevConfig.get('active_project') === msg.active_project) {
-          return prevConfig
-        }
-        
-        const newConfig = new Map(prevConfig)
-        newConfig.set('active_project', msg.active_project)
-        return newConfig
+      setGlobalConfig({
+        activeProject: msg.active_project,
+        eegPort: msg.eeg_port,
+        eegDevice: msg.eeg_device,
+        turbolinkSamplingFrequency: msg.turbolink_sampling_frequency,
+        turbolinkEegChannelCount: msg.turbolink_eeg_channel_count,
+        maximumDroppedSamples: msg.maximum_dropped_samples,
+        simulateLabjack: msg.simulate_labjack,
+        minimumIntertrialInterval: msg.minimum_intertrial_interval,
+        maximumLoopbackLatency: msg.maximum_loopback_latency,
+        maximumTimingError: msg.maximum_timing_error,
+        diskWarningThreshold: msg.disk_warning_threshold,
+        diskErrorThreshold: msg.disk_error_threshold,
       })
     })
 
@@ -66,11 +118,47 @@ export const GlobalConfigProvider: React.FC<GlobalConfigProviderProps> = ({ chil
     setParameterRos('active_project', project, callback || noop, 'global_configurator')
   }
 
+  const setGlobalConfigParams = async (
+    config: Partial<Omit<GlobalConfigContextType, 'setActiveProject' | 'setGlobalConfig'>>,
+    callback?: () => void
+  ): Promise<void> => {
+    const { setParametersRos } = await import('../ros/parameters')
+    
+    const parameters: Array<{ name: string; value: boolean | number | string }> = []
+    
+    if (config.activeProject !== undefined) parameters.push({ name: 'active_project', value: config.activeProject })
+    if (config.eegPort !== undefined) parameters.push({ name: 'eeg_port', value: config.eegPort })
+    if (config.eegDevice !== undefined) parameters.push({ name: 'eeg_device', value: config.eegDevice })
+    if (config.turbolinkSamplingFrequency !== undefined) parameters.push({ name: 'turbolink_sampling_frequency', value: config.turbolinkSamplingFrequency })
+    if (config.turbolinkEegChannelCount !== undefined) parameters.push({ name: 'turbolink_eeg_channel_count', value: config.turbolinkEegChannelCount })
+    if (config.maximumDroppedSamples !== undefined) parameters.push({ name: 'maximum_dropped_samples', value: config.maximumDroppedSamples })
+    if (config.simulateLabjack !== undefined) parameters.push({ name: 'simulate_labjack', value: config.simulateLabjack })
+    if (config.minimumIntertrialInterval !== undefined) parameters.push({ name: 'minimum_intertrial_interval', value: config.minimumIntertrialInterval })
+    if (config.maximumLoopbackLatency !== undefined) parameters.push({ name: 'maximum_loopback_latency', value: config.maximumLoopbackLatency })
+    if (config.maximumTimingError !== undefined) parameters.push({ name: 'maximum_timing_error', value: config.maximumTimingError })
+    if (config.diskWarningThreshold !== undefined) parameters.push({ name: 'disk_warning_threshold', value: config.diskWarningThreshold })
+    if (config.diskErrorThreshold !== undefined) parameters.push({ name: 'disk_error_threshold', value: config.diskErrorThreshold })
+    
+    setParametersRos(parameters, callback || noop, 'global_configurator')
+  }
+
   return (
     <GlobalConfigContext.Provider
       value={{
-        activeProject,
+        activeProject: globalConfig.activeProject,
+        eegPort: globalConfig.eegPort,
+        eegDevice: globalConfig.eegDevice,
+        turbolinkSamplingFrequency: globalConfig.turbolinkSamplingFrequency,
+        turbolinkEegChannelCount: globalConfig.turbolinkEegChannelCount,
+        maximumDroppedSamples: globalConfig.maximumDroppedSamples,
+        simulateLabjack: globalConfig.simulateLabjack,
+        minimumIntertrialInterval: globalConfig.minimumIntertrialInterval,
+        maximumLoopbackLatency: globalConfig.maximumLoopbackLatency,
+        maximumTimingError: globalConfig.maximumTimingError,
+        diskWarningThreshold: globalConfig.diskWarningThreshold,
+        diskErrorThreshold: globalConfig.diskErrorThreshold,
         setActiveProject,
+        setGlobalConfig: setGlobalConfigParams,
       }}
     >
       {children}

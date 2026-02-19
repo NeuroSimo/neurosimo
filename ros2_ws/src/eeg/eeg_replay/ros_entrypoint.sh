b819662a3a5be9d60c4091d9f20a272b5e658ba7
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2026-02-19 12:18:53 +0100

    REF: Clean up directory structure

--- ros2_ws/src/eeg/eeg_replay/ros_entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-source /opt/ros/iron/setup.bash
-source /app/ros2_ws/install/setup.bash
-
-ros2 launch eeg_replay eeg_replay.launch.py log-level:="$ROS_LOG_LEVEL"
---
+++ src/eeg/eeg_replay/ros_entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+source /opt/ros/iron/setup.bash
+source /app/install/setup.bash
+
+ros2 launch eeg_replay eeg_replay.launch.py log-level:="$ROS_LOG_LEVEL"

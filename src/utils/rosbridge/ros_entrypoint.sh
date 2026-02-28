aa5db6d4b8dd9588b42c133f2594871eaea93edf
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2026-02-28 10:44:24 +0200

    FIX: Various other fixes for Ubuntu 24

--- src/utils/rosbridge/ros_entrypoint.sh
+++ src/utils/rosbridge/ros_entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-source /opt/ros/iron/setup.bash
+source /opt/ros/jazzy/setup.bash
 source /app/install/setup.bash
 
 ros2 launch rosbridge_server rosbridge_websocket_launch.xml port:=$ROSBRIDGE_PORT

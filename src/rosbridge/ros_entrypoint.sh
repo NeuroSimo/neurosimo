e41041cdde56e1122502ec5980c69e2b779574a4
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2026-03-30 11:21:09 +0300

    FIX: Avoid exposing high-frequency topics to ROS bridge

--- src/rosbridge/ros_entrypoint.sh
+++ src/rosbridge/ros_entrypoint.sh
@@ -4,4 +4,47 @@ set -e
 source /opt/ros/jazzy/setup.bash
 source /app/install/setup.bash
 
-ros2 launch rosbridge_server rosbridge_websocket_launch.xml port:=$ROSBRIDGE_PORT
+TOPICS_GLOB="\"[\
+'/neurosimo/*/heartbeat',\
+'/neurosimo/*/health',\
+'/neurosimo/eeg_bridge/state',\
+'/neurosimo/eeg_simulator/state',\
+'/neurosimo/eeg_simulator/dataset/list',\
+'/neurosimo/session/state',\
+'/neurosimo/session_exporter/state',\
+'/neurosimo/session_configurator/config',\
+'/neurosimo/global_configurator/config',\
+'/neurosimo/pipeline/*',\
+'/neurosimo/experiment/protocol/list',\
+'/neurosimo/recording/recordings/list',\
+'/neurosimo/system/disk_status',\
+'/neurosimo/eeg_device/info',\
+'/neurosimo/eeg/statistics',\
+'/eeg_bridge/dropped_samples',\
+'/rosout'\
+]\""
+
+SERVICES_GLOB="\"[\
+'/neurosimo/session_configurator/set_parameters',\
+'/neurosimo/session_configurator/get_parameters',\
+'/neurosimo/global_configurator/set_parameters',\
+'/neurosimo/global_configurator/get_parameters',\
+'/neurosimo/projects/list',\
+'/neurosimo/projects/create',\
+'/neurosimo/session/start',\
+'/neurosimo/session/abort',\
+'/neurosimo/session/export',\
+'/neurosimo/recording_manager/recording/get_info',\
+'/neurosimo/recording_manager/recording/delete',\
+'/neurosimo/eeg_simulator/dataset/get_info',\
+'/neurosimo/experiment/pause',\
+'/neurosimo/experiment/resume',\
+'/neurosimo/experiment_coordinator/protocol/get_info',\
+'/rosapi/*'\
+]\""
+
+ros2 launch rosbridge_server rosbridge_websocket_launch.xml \
+  port:=$ROSBRIDGE_PORT \
+  topics_glob:="$TOPICS_GLOB" \
+  services_glob:="$SERVICES_GLOB" \
+  params_glob:="[]"

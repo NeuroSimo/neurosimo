d587f332b1fc3af034b6ca06f1866596dae0614b
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2026-03-30 12:18:41 +0300

    REF: Inline and remove experiment_coordinator_node.cpp

--- src/experiment_coordinator/src/experiment_coordinator.cpp
+++ src/experiment_coordinator/src/experiment_coordinator.cpp
@@ -610,3 +610,13 @@ double ExperimentCoordinator::get_experiment_time(double sample_time) {
   return experiment_time;
 }
 
+int main(int argc, char *argv[]) {
+  rclcpp::init(argc, argv);
+
+  auto node = std::make_shared<ExperimentCoordinator>();
+
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+
+  return 0;
+}

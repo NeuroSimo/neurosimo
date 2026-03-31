6d6b48cb31973b1ce9f4e7484a307e7924f631cb
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2026-03-31 19:10:17 +0300

    MOD: Drop allowlist from rosbridge launch arguments
    Doesn't seem to give performance benefits.

--- src/rosbridge/ros_entrypoint.sh
+++ src/rosbridge/ros_entrypoint.sh
@@ -4,51 +4,7 @@ set -e
 source /opt/ros/jazzy/setup.bash
 source /app/install/setup.bash
 
-TOPICS_GLOB="\"[\
-'/neurosimo/*/heartbeat',\
-'/neurosimo/*/health',\
-'/neurosimo/eeg_bridge/state',\
-'/neurosimo/eeg_simulator/state',\
-'/neurosimo/eeg_simulator/dataset/list',\
-'/neurosimo/session/state',\
-'/neurosimo/session_exporter/state',\
-'/neurosimo/session_configurator/config',\
-'/neurosimo/global_configurator/config',\
-'/neurosimo/pipeline/*',\
-'/neurosimo/experiment/protocol/list',\
-'/neurosimo/recording/recordings/list',\
-'/neurosimo/system/disk_status',\
-'/neurosimo/eeg_device/info',\
-'/neurosimo/eeg/statistics',\
-'/neurosimo/eeg/dropped_samples',\
-'/rosout'\
-]\""
-
-SERVICES_GLOB="\"[\
-'/neurosimo/session_configurator/set_parameters',\
-'/neurosimo/session_configurator/get_parameters',\
-'/neurosimo/global_configurator/set_parameters',\
-'/neurosimo/global_configurator/get_parameters',\
-'/neurosimo/projects/list',\
-'/neurosimo/projects/create',\
-'/neurosimo/session/start',\
-'/neurosimo/session/abort',\
-'/neurosimo/session/export',\
-'/neurosimo/recording_manager/recording/get_info',\
-'/neurosimo/recording_manager/recording/delete',\
-'/neurosimo/eeg_simulator/dataset/get_info',\
-'/neurosimo/experiment/pause',\
-'/neurosimo/experiment/resume',\
-'/neurosimo/experiment_coordinator/protocol/get_info',\
-'/rosapi/*'\
-]\""
-
-PARAMS_GLOB="\"[]\""
-
 ros2 launch rosbridge_server rosbridge_websocket_launch.xml \
   port:=$ROSBRIDGE_PORT \
   namespace:=neurosimo \
-  use_events_executor:=true \
-  topics_glob:="$TOPICS_GLOB" \
-  services_glob:="$SERVICES_GLOB" \
-  params_glob:="$PARAMS_GLOB"
+  use_events_executor:=true

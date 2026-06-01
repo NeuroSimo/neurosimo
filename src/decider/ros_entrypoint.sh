628d4f2c888c0f01982ecf656ff2d60a7c7bc7a9
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2026-06-01 08:07:04 +0300

    MOD: Set OMP, NUMEXPR, and MKL num of threads to 1

--- src/decider/ros_entrypoint.sh
+++ src/decider/ros_entrypoint.sh
@@ -17,9 +17,13 @@ set -e
 source /opt/ros/jazzy/setup.bash
 source /app/install/setup.bash
 
-# The default number of threads used by OpenBLAS is the number of cores; this seems to slow down
-# NumPy operations, whereas setting it to a smaller value such as 1 seems to speed them up.
+# The default number of threads used by OpenBLAS is the number of cores; multi-threaded reductions by
+# BLAS/OpenMP are non-deterministic, so setting it to 1 improves determinism, and also seems to make
+# NumPy operations faster by avoiding thread overhead.
 export OPENBLAS_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export OMP_NUM_THREADS=1
+export NUMEXPR_NUM_THREADS=1
 
 while true; do
     ros2 launch decider decider.launch.py log-level:="$ROS_LOG_LEVEL"

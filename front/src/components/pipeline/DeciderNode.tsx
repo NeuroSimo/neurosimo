3dfaf3bbeee58143b72ca7aaf21666e303d38341
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2026-06-11 17:33:51 +0300

    MOD: Show 'disabled' or 'bypassed' in PipelineView

--- front/src/components/pipeline/DeciderNode.tsx
+++ front/src/components/pipeline/DeciderNode.tsx
@@ -29,6 +29,7 @@ export const DeciderNode: React.FC = () => {
       onToggle={handleToggle}
       onModuleChange={handleModuleChange}
       folderName="decider"
+      disabledLabel="DISABLED"
     />
   )
 }
